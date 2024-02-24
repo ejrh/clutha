459cd9baed1e382b9ccc8daad6340e27912b3c0c
ejrh <ejrh00@gmail.com>
2024-02-24 17:15:31 +1300

    Shut down if given the !shutdown command

--- src/discord.rs
+++ src/discord.rs
@@ -1,3 +1,6 @@
+use std::sync::Arc;
+
+use serenity::all::ShardManager;
 use serenity::async_trait;
 use serenity::model::channel::Message;
 use serenity::model::gateway::Ready;
@@ -6,9 +9,31 @@ use serenity::utils::MessageBuilder;
 
 struct Handler;
 
+struct ShardManagerContainer(Arc<ShardManager>);
+
+impl TypeMapKey for ShardManagerContainer {
+    type Value = Arc<ShardManager>;
+}
+
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, context: Context, msg: Message) {
+        if msg.content == "!shutdown" {
+            let response = MessageBuilder::new()
+                .push("Shutting down")
+                .build();
+
+            if let Err(why) = msg.channel_id.say(&context.http, &response).await {
+                println!("Error sending message: {why:?}");
+            }
+
+            {
+                let data = context.data.read().await;
+                if let Some(shard_manager) = data.get::<ShardManagerContainer>() {
+                    shard_manager.shutdown_all().await;
+                }
+            }
+        }
         if msg.content == "!ping" {
             let channel = match msg.channel_id.to_channel(&context).await {
                 Ok(channel) => channel,
@@ -43,6 +68,11 @@ pub(crate) async fn say_hello(token: &str) -> Result<(), serenity::Error> {
         | GatewayIntents::MESSAGE_CONTENT;
     let mut client =
         Client::builder(&token, intents).event_handler(Handler).await?;
+
+    {
+        let mut data = client.data.write().await;
+        data.insert::<ShardManagerContainer>(Arc::clone(&client.shard_manager));
+    }
 
     client.start().await?;
 
